1762447b50c4b77b45abe89a6194a4a20f6009f6
ldm0805 <leonardodimaggio94@gmail.com>
2023-01-10 09:49:21 +0100

    semplificazione v-for

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ createApp({
         clickEmailRandom(){
             // Ciclo per inserire le mail nell'array vuoto.
             for(let i = 0; i < 10; i++) {
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result)=> {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
                     this.email = result.data.response;
                     this.emailListArray.push(this.email);
                 })
